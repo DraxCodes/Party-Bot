9a8670d7e6c6424d6d5e6ddc1fb23d4d2009b0b6
Joel Parkinson <joelparkinson@live.co.uk>
2018-12-23 10:41:29 +0000

    Added BotService For General Commands

---
+++ PartyBot/Modules/AdminModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using PartyBot.Services;
+using System.Threading.Tasks;
+
+namespace PartyBot.Modules
+{
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        public BotService BotService { get; set; }
+        public AudioService AudioService { get; }
+
+        [Command("Info")]
+        public async Task Info()
+            => await ReplyAsync("", false, await BotService.DisplayInfoAsync(Context));
+
+        [Command("Prefix")]
+        public async Task Prefix()
+        {
+
+        }
+    }
+}
---
+++ PartyBot/Services/BotService.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using PartyBot.Handlers;
+using System.Linq;
+using System.Collections.Generic;
+using Victoria;
+
+namespace PartyBot.Services
+{
+    public sealed class BotService
+    {
+        public AudioService Audio { get; set; }
+
+        public async Task<Embed> DisplayInfoAsync(SocketCommandContext context)
+        {
+            var fields = new List<EmbedFieldBuilder>();
+            fields.Add(new EmbedFieldBuilder {
+                Name = "Client Info",
+                Value = $"Current Server: {context.Guild.Name} - Prefix: {Global.Config.DefaultPrefix}",
+                IsInline = false
+            });
+            fields.Add(new EmbedFieldBuilder {
+                Name = "Guild Info",
+                Value = $"Current People: {context.Guild.Users.Count(x => !x.IsBot)} - Current Bots: {context.Guild.Users.Count(x => x.IsBot)} - Overall Users: {context.Guild.Users.Count}\n" +
+                $"Text Channels: {context.Guild.TextChannels.Count} - Voice Channels: {context.Guild.VoiceChannels.Count}",
+                IsInline = false
+            });
+
+            var embed = await Task.Run(() => new EmbedBuilder
+            {
+                Title = $"Info",
+                ThumbnailUrl = context.Guild.IconUrl,
+                Timestamp = DateTime.UtcNow,
+                Color = Color.DarkOrange,
+                Footer = new EmbedFooterBuilder { Text = "Powered By DraxCodes PartyBot & Victoria", IconUrl = context.Client.CurrentUser.GetAvatarUrl() },
+                Fields = fields
+            });
+
+            return embed.Build();
+        }
+
+    }
+}
