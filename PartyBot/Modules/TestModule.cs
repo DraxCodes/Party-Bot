0c0b3a67159b3719048f90553336174d05a571a9
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-29 21:27:23 +0000

    Random Test Module {DELETE}

---
+++ PartyBot/Modules/TestModule.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyBot.Modules
+{
+    public class TestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task test(SocketGuildUser user)
+        {
+            var message = await Context.Channel.GetMessageAsync(552567071490310165) as IUserMessage;
+            var reactions = message.Reactions;
+            foreach (var r in reactions)
+            {
+                await message.RemoveReactionAsync(r.Key, user as IUser);
+            }
+        }
+    }
+}
