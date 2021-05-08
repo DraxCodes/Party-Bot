e37e7fec93602ceaae0ba6e632fb5065ab5ddac5
CarrC2021 <ccarr2017@gmail.com>
2021-05-08 12:49:39 -0700

    Need to read into how to automatically respond to events

---
+++ PartyBot/Handlers/FileHandler.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PartyBot.Handlers
+{
+    public class Filehandler
+    {
+        private readonly DiscordSocketClient _client;
+        /* This file is where we can store all the File Helper Methods (So to speak). 
+             We want to asynchronously Deserialize Json files and then store this info somewhere. 
+             We should probably do our best to keep these methods private. */
+
+
+        private async Task DeserializeJson(SocketMessage socketMessage)
+        {
+            if (socketMessage.Attachments.Any())
+            {
+                await socketMessage.Channel.SendMessageAsync(socketMessage.Attachments.First().ToString());
+            }
+            
+        }
+
+    }
+}
